717b4ac2cdbcf5cd579e649d9cc8b879a9de2951
micahspencer-png <micahspencer@isu.edu>
2026-05-06 12:54:51 -0600

    added files with bash

---
+++ HVAC_Control/HVAC_Control.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HVACSmartHomeController
+{
+    partial class HVACSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.QYAT_Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(724, 51);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(114, 48);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Location = new System.Drawing.Point(724, 12);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(114, 33);
+            this.PortComboBox.TabIndex = 1;
+            // 
+            // QYAT_Timer
+            // 
+            this.QYAT_Timer.Tick += new System.EventHandler(this.QYAT_Timer_Tick);
+            // 
+            // HVACSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 491);
+            this.Controls.Add(this.PortComboBox);
+            this.Controls.Add(this.ExitButton);
+            this.MaximizeBox = false;
+            this.Name = "HVACSystem";
+            this.Text = "HVAC Window";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.Timer QYAT_Timer;
+    }
+}
+
